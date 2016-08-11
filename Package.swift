f6f0b2a3fa42fa2b637cfca3e1f5a36c758c5506
noppoman <yuki@miketokyo.com>
2016-08-12 03:13:49 +0900

    Updating to August 4, 2016

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "HTTPUpgradeAsync",
   	dependencies: [
-      .Package(url: "https://github.com/open-swift/S4.git", majorVersion: 0, minor: 9)
+      .Package(url: "https://github.com/noppoMan/S4.git", majorVersion: 0, minor: 12)
    ]
 )
